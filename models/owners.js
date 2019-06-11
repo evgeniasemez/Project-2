42fbdb22dcff9f3e818daf771e71f5f79be9cc86
Alex Poplawski <alex0poplawski@gmail.com>
2019-06-10 21:48:49 -0700

    put sql tables into sequelize

---
+++ models/owners.js
@@ -0,0 +1,39 @@
+module.exports = function(sequelize, DataTypes) {
+  var owners = sequelize.define("owners", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  owners.associate = function(models) {
+    owners.hasMany(models.dogs, {
+      onDelete: "cascade"
+    });
+  };
+  return owners;
+};
