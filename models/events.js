afc86e9a0f46f3d47fac10f83b15bd8d085df3b2
Alex Poplawski <alex0poplawski@gmail.com>
2019-06-18 10:41:48 -0700

    model changes and force a resync on herokus end

--- models/events.js
+++ models/events.js
@@ -1,28 +1,24 @@
 module.exports = function(sequelize, DataTypes) {
-  var events = sequelize.define(
-    "events",
-    {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      location: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
-      //TODO: log / lat?
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false
+  var events = sequelize.define("events", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
       }
     },
-    { timestamps: false }
-  );
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    //TODO: log / lat?
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  });
   return events;
 };
